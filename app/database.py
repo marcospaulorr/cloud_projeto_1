940dda7ba36f16da0316af8910bf17efe742d08c
marcospaulorr <marcospauloricartereboucas@gmail.com>
2025-05-23 19:23:23 -0300

    alguem me ajuda

--- app/database.py
+++ app/database.py
@@ -1,6 +1,10 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+# logo no início, antes de usar os os.getenv()
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
