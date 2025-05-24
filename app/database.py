d653e5d5ba30487bbcf98414148c3768d91e7985
marcospaulorr <marcospauloricartereboucas@gmail.com>
2025-05-24 01:58:27 -0300

    versao final

--- app/database.py
+++ app/database.py
@@ -1,12 +1,21 @@
+# app/database.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-# logo no início, antes de usar os os.getenv()
+# Carrega as variáveis de ambiente
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Constrói a DATABASE_URL dinamicamente
+DB_USER = os.getenv("DB_USER", "projeto")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "projeto123")
+DB_HOST = os.getenv("DB_HOST", "db")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "projeto")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
